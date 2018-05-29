20b8d39123d2a69e688676a36fd3146b95d53d55
jeay <wrjie@msn.cn>
2018-05-29 17:35:16 +0800

    错误捕捉

--- conv.py
+++ conv.py
@@ -6,7 +6,7 @@
  * @Author: jeay 
  * @Date: 2018-05-29 16:11:38 
  * @Last Modified by: jeay
- * @Last Modified time: 2018-05-29 16:13:47
+ * @Last Modified time: 2018-05-29 17:33:37
  */
 '''
 
@@ -28,18 +28,21 @@ def convert(filename, out_enc = "UTF8"):
             fp = open(filename, 'r', encoding=coding)
             content = fp.read()
             fp.close()
-            new_content = content.encode(out_enc)
+            try:
+                new_content = content.encode(out_enc)
+            except: 
+                return filename + " error"
             fp = open(filename, 'wb')
             fp.write(new_content)
             fp.close()
             return filename + " done"
-            pass
+            #pass
             #print(" done")
         else:
             return filename + " already "+coding
-            pass
+            #pass
             #print(coding)
     except IOError:
         return filename + " error "
-        pass
+        #pass
         #print(" error")
